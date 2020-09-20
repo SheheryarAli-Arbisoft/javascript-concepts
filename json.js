2ba7983961a9d80ce72d2285b7f499f6621e90c5
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-20 20:37:11 +0500

    Added JSON code

---
+++ json.js
@@ -0,0 +1,14 @@
+// // JSON.stringify() is used to convert an object to a JSON string
+// const obj = {
+//   name: 'Sheheryar',
+//   designation: 'Software Engineer',
+// };
+
+// const jsonString = JSON.stringify(obj);
+// console.log(jsonString);
+
+// // JSON.parse() is used to create an object from a JSON string
+// const jsonString = '{"name":"Sheheryar","designation":"Software Engineer"}';
+
+// const obj = JSON.parse(jsonString);
+// console.log(obj);
