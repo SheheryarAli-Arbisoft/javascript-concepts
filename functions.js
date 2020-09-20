e9022912e5559cce55313a491fb3bdfff3ab1c9e
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-20 23:54:48 +0500

    Added functions code

---
+++ functions.js
@@ -0,0 +1,20 @@
+// // Function declaration
+// sayHi();
+
+// function sayHi() {
+//   console.log('Hello World');
+// }
+
+// // Function expression
+// const sayHi = function () {
+//   console.log('Hello World');
+// };
+
+// sayHi();
+
+// // Arrow function
+// const sayHi = () => {
+//   console.log('Hello World');
+// };
+
+// sayHi();
