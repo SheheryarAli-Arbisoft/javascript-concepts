aa925215a96f3ce7981a939748dba5ab2397aaa2
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-21 00:58:40 +0500

    Added maps code

---
+++ map.js
@@ -0,0 +1,23 @@
+// // Normal Map
+// let user = { name: 'Sheheryar' };
+
+// const map = new Map();
+// map.set(user, 2);
+
+// console.log(map.get(user));
+
+// user = null;
+// console.log(map.get(user));
+// console.log(user);
+
+// // Weak Map
+// let user = { name: 'Sheheryar' };
+
+// const map = new WeakMap();
+// map.set(user, 2);
+
+// console.log(map.get(user));
+
+// user = null;
+// console.log(map.get(user));
+// console.log(user);
