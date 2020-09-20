be676694e7025de3021ddd317165f076c0f955bd
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-20 20:21:19 +0500

    Added conditionals code

---
+++ conditionals.js
@@ -0,0 +1,26 @@
+// // If else
+// let x = 1;
+
+// // In this conditional we can check equality based on value and type by using the appropriate operator (==, ===, !=, !==)
+// if (x == 1) {
+//   console.log('One');
+// } else if (x == 2) {
+//   console.log('Two');
+// } else {
+//   console.log('Default');
+// }
+
+// // Switch
+// let x = 1;
+
+// // In this conditional the equality is check based on value and type only (===, !==)
+// switch (x) {
+//   case 1:
+//     console.log('One');
+//     break;
+//   case 2:
+//     console.log('Two');
+//     break;
+//   default:
+//     console.log('Default');
+// }
