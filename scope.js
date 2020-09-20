34155a711627d8b52a74dd9b8e22e4f3530b06c3
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-20 20:49:43 +0500

    Added scope code

---
+++ scope.js
@@ -0,0 +1,29 @@
+// // Global scope
+// var greeting = 'Hello World';
+
+// function sayHi() {
+//   console.log(greeting);
+// }
+
+// sayHi();
+
+// // Local / Function scope
+// function sayHi() {
+//   var greeting = 'Hello World';
+//   console.log(greeting);
+// }
+
+// console.log(greeting);
+
+// // Block scope
+// function sayHi(x) {
+//   if (x == 1) {
+//     let greeting = 'Hello World';
+//   } else {
+//     const greeting = 'Good Bye';
+//   }
+
+//   console.log(greeting);
+// }
+
+// sayHi(1);
