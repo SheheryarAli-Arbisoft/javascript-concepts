807f425dba40b4249e7c502fadd1f381b02e52a1
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-20 20:24:54 +0500

    Added loops code

---
+++ loops.js
@@ -0,0 +1,20 @@
+// // For loop
+// for (let i = 0; i < 5; i++) {
+//   console.log(i + 1);
+// }
+
+// // While loop
+// let i = 0;
+
+// while (i < 5) {
+//   console.log(i + 1);
+//   i++;
+// }
+
+// // Do while loop
+// let i = 5;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
