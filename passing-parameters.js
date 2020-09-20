f2f15c7e6ade3a7ace706ed51c0deeb4f056dabc
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-20 21:32:14 +0500

    Added passing paramters code

---
+++ passing-parameters.js
@@ -0,0 +1,25 @@
+// // Pass by value
+// function test(x) {
+//   x = 10;
+//   console.log('Inside function: ', x);
+// }
+
+// let x = 5;
+
+// console.log('\nBefore function: ', x);
+// test(x);
+// console.log('After function: ', x);
+// console.log('\n');
+
+// // Pass by reference
+// function test(obj) {
+//   obj.age = 30;
+//   console.log('Inside function: ' + obj.age);
+// }
+
+// let obj = { age: 22 };
+
+// console.log('\nBefore function: ' + obj.age);
+// test(obj);
+// console.log('After function: ' + obj.age);
+// console.log('\n');
