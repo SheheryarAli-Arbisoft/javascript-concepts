2a7a60da57104a85427d31041a7c7b319d3ffbf2
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-20 20:56:29 +0500

    Added hoisting code

---
+++ hoisting.js
@@ -0,0 +1,22 @@
+// // Functions are hoisted during compilation to the top of the document. Function expressions and arrow funtions are not hoisted
+// sayHi();
+
+// function sayHi() {
+//   console.log('Hello World');
+// }
+
+// Variable declarations with 'var' are hoisted during compilation to the top of the document but the initialization is not changed. Variable declarations with 'let' and 'const' are not hoisted
+
+// // Example 1
+// console.log(num);
+// var num = 6;
+
+// // Example 2
+// num = 6;
+// console.log(num);
+// var num;
+
+// // Example 3
+// num = 6;
+// console.log(num);
+// let num;
