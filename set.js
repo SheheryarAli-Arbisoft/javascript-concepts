09e12ec047e8d506ee3ba96ca3135cf30929b79a
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-21 01:08:54 +0500

    Added set code

---
+++ set.js
@@ -0,0 +1,23 @@
+// // Normal set
+// let user = { name: 'Sheheryar' };
+
+// const set = new Set();
+// set.add(user);
+
+// console.log(set);
+
+// user = null;
+// console.log(set);
+// console.log(user);
+
+// // Weak set
+// let user = { name: 'Sheheryar' };
+
+// const set = new WeakSet();
+// set.add(user);
+
+// console.log(set.has(user));
+
+// user = null;
+// console.log(set.has(user));
+// console.log(user);
