e8adfab55e271a176f7401eaf2d7b16bc8786f8d
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-21 11:53:51 +0500

    Added strings code

---
+++ strings.js
@@ -0,0 +1,13 @@
+let name = 'Sheheryar';
+
+console.log(name);
+console.log(name[2]);
+
+name[2] = 'a';
+console.log(name);
+
+console.log(name.toUpperCase());
+
+for (char of name) {
+  console.log(char);
+}
