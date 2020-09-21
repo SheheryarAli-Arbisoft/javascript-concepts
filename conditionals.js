e82aa8eaf7ee17e57a60c7e1e88f17419371ad35
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-21 12:41:03 +0500

    Added comments in conditionals code

--- conditionals.js
+++ conditionals.js
@@ -1,26 +1,33 @@
-// // If else
-// let x = 1;
+let x = 1;
 
-// // In this conditional we can check equality based on value and type by using the appropriate operator (==, ===, !=, !==)
-// if (x == 1) {
-//   console.log('One');
-// } else if (x == 2) {
-//   console.log('Two');
-// } else {
-//   console.log('Default');
-// }
+/*
+ * The if else conditional is used when we want to compare values using
+ * any of the comparison operators (<, >, <=. >=, ==, ===, !=, !==)
+ * and check any condition using the logical operators (&&, ||, !)
+ */
+if (x == 1) {
+  console.log('One');
+} else if (x == 2) {
+  console.log('Two');
+} else {
+  console.log('Default');
+}
 
-// // Switch
-// let x = 1;
-
-// // In this conditional the equality is check based on value and type only (===, !==)
-// switch (x) {
-//   case 1:
-//     console.log('One');
-//     break;
-//   case 2:
-//     console.log('Two');
-//     break;
-//   default:
-//     console.log('Default');
-// }
+/*
+ * The switch conditional is used when we want to compare values using only the strict
+ * equality operator (===)
+ *
+ * Note: In the example below if we were to write '1' instead of 1 then the default case
+ * would be entered because strict equality check on the basis of logical value and also
+ * by data type
+ */
+switch (x) {
+  case 1:
+    console.log('One');
+    break;
+  case 2:
+    console.log('Two');
+    break;
+  default:
+    console.log('Default');
+}
