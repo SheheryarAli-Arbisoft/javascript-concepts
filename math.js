9a4caf67eecbba74e39212e24cd188d43e314b4c
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-09-22 23:15:38 +0500

    Added math object code

---
+++ math.js
@@ -0,0 +1,18 @@
+/*
+ * JavaScript has a built-in Math class which contains many helpful methods.
+ */
+
+// There are many already provided methods by the Math class. Some of them are mentioned below.
+console.log(Math.round(3.14));
+console.log(Math.pow(2, 3));
+console.log(Math.ceil(1.1));
+console.log(Math.floor(1.1));
+console.log(Math.sqrt(2));
+console.log(Math.abs(-3));
+console.log(Math.sin(90));
+
+// There are also many predefined constants in the Math class. Some of them are mentioned below.
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.E);
+console.log(Math.LOG10E);
